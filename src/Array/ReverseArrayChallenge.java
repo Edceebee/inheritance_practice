4a4490d38725ae49f59d1b2517ffacf458bfb2a7
Chinonso Edozie <edceebee@gmail.com>
2021-03-19 11:53:34 +0100

    updated

---
+++ src/Array/ReverseArrayChallenge.java
@@ -0,0 +1,12 @@
+package Array;
+/*
+1. write a method called reverse() with an int array as a parameter
+2. the method should not return any value, in other words the method is allowed to modify the parameter.
+3. in the main() test the reverse() method, and print the array both reversed and non reversed.
+4. to reverse the array,yo have to swap the elements , so that the first element is swapped with the last element and so on.
+
+ */
+
+
+public class ReverseArrayChallenge {
+}
